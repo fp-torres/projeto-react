e7fcb249c96808bff57a6491dd7785bdf2bceb12
Felipe Torres <felipetorresaraujo@gmail.com>
2025-11-13 19:36:14 -0300

    React Icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,16 @@
+import {FaFacebook, FaLinkedin, FaInstagram,} from 'react-icons/fa';
+
+import styles from './Footer.module.css'; 
+
 function Footer() {
   return (
     <footer>
+      <ul className={styles.social_list}> 
+      <li><FaFacebook /></li>
+      <li><FaInstagram /></li>
+      <li><FaLinkedin /></li>
+      </ul>
+     
       <p>© 2024 My Company. All rights reserved.</p>
     </footer>
   );
