fe90079c3ddb0da491ff485f838383c25c147673
Felipe Torres <felipetorresaraujo@gmail.com>
2025-11-18 22:41:58 -0300

    Resgatando projetos do banco de dados

---
+++ costs/src/components/project/ProjectCard.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import styles from './ProjectCard.module.css';
+
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
+
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+    return (
+        <div className={styles.project_card}>
+            <h4>{name}</h4>
+            <p>
+                <span>Orçamento:</span> R$ {budget}
+            </p>
+            <p className={styles.category_text}>
+                <span className={`${styles.category} ${styles[category.toLowerCase()]}`}></span> {category}
+            </p>
+            <div className={styles.project_card_actions}>
+                {/* AQUI MUDOU: O Link é o próprio botão agora, sem wrapper */}
+                <Link to={`/project/${id}`}>
+                    <BsPencil /> Editar
+                </Link>
+                
+                <button onClick={handleRemove}>
+                    <BsFillTrashFill /> Excluir
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectCard;
